08f50cb2ffa3db6f32929eef7553794185ba1262
Elyas Sepahi <elyassepahi@Mac.broadband>
2025-02-20 14:45:08 +0000

    Updated main.py with new changes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import keras
+import tensorflow as ts
 import numpy as np
 import cv2 as cv
 from tkinter import *
 
 #colours
+lightGrey = "#D9D9D9"
+grey = "#3A3A3A"
+darkGrey = "#212121"
+blue = "#6E6C80"
 
+#fonts
+Font = ("Arabic Transparent", 15)
+FontBold = ("Arabic Transparent", 15)
 
-
-
-
-# signify class, tkinter window
+#signify class
