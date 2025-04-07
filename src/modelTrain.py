0c64c8d034ebdc37e61ed05f65a6419b3e9f398e
Elyas Sepahi <76588047+Swiftal13@users.noreply.github.com>
2025-04-07 22:35:03 +0100

    modelTrain.py addittion

--- src/modelTrain.py
+++ src/modelTrain.py
@@ -1,11 +1,18 @@
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from dataPreprocess import load_data 
+from dataPreprocess import loadData 
+import time
+
+
 
 # Load the data
-trainData, testData = load_data(r'D:\Programming\Python\All PY files\SignifyNEA\NEA\Datasets\train', 
+trainData, testData = loadData(r'D:\Programming\Python\All PY files\SignifyNEA\NEA\Datasets\train', 
                                   r'D:\Programming\Python\All PY files\SignifyNEA\NEA\Datasets\test')
 
+
+sampleBatch, sampleLabels = next(trainData)  # Get a sample batch from the training data generator
+print("Shape of first batch of images:", sampleBatch.shape)  # Should print (batch_size, height, width, channels)
+print("Shape of first batch of labels:", sampleLabels.shape)  # Should print (batch_size, num_classes)
 
 # CNN model architecture
 def buildModel(inputShape, numClasses):
@@ -20,10 +27,16 @@ def buildModel(inputShape, numClasses):
     return model
 
 # model architecture
-model = buildModel(inputShape=(64, 64, 3), numClasses=trainData.numClasses)
+model = buildModel(inputShape=(64, 64, 3), numClasses=trainData.num_classes)
 
-# Train the model
-model.fit(trainData, epochs=10, validation_data=testData)
+
+start = time.time() # Start time for training
+
+model.fit(trainData, epochs=10, validation_data=testData) # Train the model
+
+end = time.time() # Calculate the time taken to train the model
+
+print(f"Training completed in {(end - start):.2f} seconds.") # Print the time taken to train the model
 
 # Save the trained model
 model.save('asl_model.h5')
